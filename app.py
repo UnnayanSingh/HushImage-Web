aa932aba1e6dd2f998218be20397aee65a50eaa4
Unnayan Singh <unnayansingh2005@gmail.com>
2025-09-01 23:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -220,5 +220,7 @@ def logout():
     session.clear()
     return redirect('/login')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
